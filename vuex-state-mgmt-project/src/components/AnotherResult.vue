78f6a14b5069eb5ba53df478a16548b5e462393b
tarini <tarininegi@gmail.com>
2019-01-18 07:58:16 +0530

    state mgmt using vuex

---
+++ vuex-state-mgmt-project/src/components/AnotherResult.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Another Counter is: {{ doubleCounter }}</p>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+    export default {
+        computed: {
+            ...mapGetters([
+            'doubleCounter'
+            ])
+        }
+        //props: ['counter']
+        /*computed: {
+            counter() {
+                return this.$store.getters.doubleCounter;
+            }
+        }*/
+    }
+</script>
---
+++ vuex-state-mgmt-project/src/components/Result.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Counter is: {{ doubleCounter }}</p>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+
+    export default {
+        //props: ['counter']
+        computed: mapGetters([
+            'doubleCounter'
+        ])
+         /*{
+
+           counter() {
+                return this.$store.getters.doubleCounter;  
+            }
+        }*/
+    }
+</script>
