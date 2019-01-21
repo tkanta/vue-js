7f7c974786cffa38c818a4ee2ab2999bed408e4a
Tarani Kanta Negi <taranin@ispace.com>
2019-01-21 19:41:55 +0530

    stock trader project

---
+++ stock-trader-project/src/components/stocks/Stocks.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <app-stock v-for="(stock, index) in stocks" :key="index" :stock="stock"> </app-stock>
+    </div>
+</template>
+
+<script>
+import Stock from './Stock.vue';
+
+export default {
+
+    data() {
+        return {
+            stocks: [
+                {id: 1, name: 'BMW', price: 100},
+                {id: 2, name: 'APPLE', price: 200},
+                {id: 3, name: 'Google', price: 300},
+                {id: 4, name: 'Twitter', price: 400}
+
+            ]
+        }
+    },
+
+    components: {
+        appStock: Stock
+    }
+}
+</script>
